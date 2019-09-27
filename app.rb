fccfca1547a65f69b0518fec1f51413b0c9740ab
Laura Berge <lauraeberge@gmail.com>
2019-09-27 15:42:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   configure do
     enable :sessions
     set :session_secret, "MAHIE#@($NF3&WAFHWA#@(F"
   end
+  
+  get '/' do
+    erb :index
+  end
+  
 end
